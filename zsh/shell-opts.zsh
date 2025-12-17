82c2e2719c62a4c936977703e9743ee5f2fb6e8c
Luke Van De Weghe <39035228+Gorfeywog@users.noreply.github.com>
2025-12-16 20:49:44 -0600

    Add more zsh-specific settings

--- zsh/shell-opts.zsh
+++ zsh/shell-opts.zsh
@@ -1,4 +1,33 @@
+# Line editor config
 autoload -U select-word-style
 select-word-style bash # Consider select-word-style with e.g. WORDCHARS=${WORDCHARS//[\/]}
 bindkey $terminfo[kLFT5] backward-word
 bindkey $terminfo[kRIT5] forward-word
+bindkey -e # Use emacs keybindings
+
+# History
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+setopt histignorealldups sharehistory
+
+# Completion
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
